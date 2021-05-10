fefc7e85ca5bbb1c58a5b841d95ab196125ab1e6
medbenzekri <benzekri.mohammed@univ-bechar.dz>
2021-05-10 12:59:39 +0100

    some nice simple gui

---
+++ seen/Main.java
@@ -0,0 +1,26 @@
+package seen;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.input.*;
+import javafx.scene.paint.Color;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage)  throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("seen.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Calculator :)");
+        stage.show();
+    }
+}
