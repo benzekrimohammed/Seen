b5d45f098d7937fd7ffc029f012cfc86c014edee
medbenzekri <benzekri.mohammed@univ-bechar.dz>
2021-05-16 03:33:21 +0100

    before testing to change it to the same attributes as the api response object

---
+++ seen/model/Movie.java
@@ -0,0 +1,43 @@
+package seen.model;
+
+public class Movie {
+
+    private String name;
+    private String rating;
+    private String Status;
+    private String poster;
+
+    public String getPoster() {
+        return this.poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getStatus() {
+        return this.Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+    
+
+}
